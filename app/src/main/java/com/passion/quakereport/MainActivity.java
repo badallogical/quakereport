6e0eebe74cae44a9772f486c5cb9828e9b8ed2a8
narayan <badallearn@gmail.com>
2020-10-14 14:30:37 +0530

    implemented LoaderManager.LoaderCallBack<ArrayList<EarthQuake>> , working app

--- app/src/main/java/com/passion/quakereport/MainActivity.java
+++ app/src/main/java/com/passion/quakereport/MainActivity.java
@@ -1,6 +1,10 @@
 package com.passion.quakereport;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -15,7 +19,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<EarthQuake>> {
 
     private static final String LOG = MainActivity.class.getName();
 
@@ -23,71 +27,49 @@ public class MainActivity extends AppCompatActivity {
     private static final String URL_QUERY = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
 
     ListView listView = null;
-    EarthquakeAdapter adapter = null;
+    EarthquakeAdapter adapter =null;
+
+    private final int Loader_Id = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // get the Earthquake data form json response
-        EarthQuakeAsyncRequest earthQuakeAsyncRequest = new EarthQuakeAsyncRequest();
-        earthQuakeAsyncRequest.execute(URL_QUERY);
-    }
-
-
-
-    private class EarthQuakeAsyncRequest extends AsyncTask<String, Void, ArrayList<EarthQuake>> {
-
-        @Override
-        protected void onPreExecute() {
-            listView = findViewById(R.id.list);
-        }
-
-        @Override
-        protected ArrayList<EarthQuake> doInBackground(String... jsonRequest) {
-            ArrayList<EarthQuake> list = null;
-            if( jsonRequest.length < 1 || jsonRequest[0] == null )
-                return null;
-
-            // make HttpRequest and get the EarthQuake data list
-            try {
-                list = QuakeUtils.makeRequestOfEarthQuakeData(jsonRequest[0]);
-            }
-            catch (IOException e){
-                Log.v( LOG + "Exception" , e.getMessage() );
-            }
-
-            return list;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<EarthQuake> earthQuakes) {
-            if( earthQuakes == null ){
-                return;
-            }
-
-            // fill the earthQuake data to list
-            updateUI(earthQuakes);
-        }
-    }
-
-    private void updateUI( ArrayList<EarthQuake> list ){
-
-        // put data into list
-        adapter = new EarthquakeAdapter(MainActivity.this, list );
+        // configure list and adapter
+        listView = (ListView) findViewById(R.id.list);
+        adapter = new EarthquakeAdapter(this, new ArrayList<EarthQuake>());
         listView.setAdapter( adapter );
 
-        final ArrayList<EarthQuake> innerHelperList = list;
-        listView.setOnItemClickListener( new AdapterView.OnItemClickListener(){
+        // get the Earthquake data form json response
+        getSupportLoaderManager().initLoader(Loader_Id, null, this );
+    }
 
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intentBrowser = new Intent( Intent.ACTION_VIEW);
-                intentBrowser.setData( innerHelperList.get(i).getUrl() );
-                startActivity(intentBrowser);
-            }
-        });
+    @Override
+    public Loader<ArrayList<EarthQuake>> onCreateLoader(int id,  Bundle args) {
+        return new EarthquakeLoader(this, URL_QUERY);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<ArrayList<EarthQuake>> loader, ArrayList<EarthQuake> earthQuakes) {
+       // clear the adapter data
+        adapter.clear();
+
+        if( earthQuakes != null ){
+            adapter.addAll( earthQuakes );
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent browserIntent = new Intent( Intent.ACTION_VIEW);
+                    browserIntent.setData( adapter.getItem(i).getUrl() );
+                    startActivity(browserIntent);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<ArrayList<EarthQuake>> loader) {
+        adapter.clear();
     }
 }
